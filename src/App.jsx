ce684e052080dfeaa6fc0e61180f34862af5dbe9
Cameron Stanley <cam96stanley@gmail.com>
2026-01-13 02:22:40 -0500

    created basic layout in App.jsx for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Auth from "./pages/auth/Auth";
+import Dashboard from "./pages/dashboard/Dashboard";
+
 function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Auth />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <>
+      <p>Navbar</p>
+
+      <Outlet />
+    </>
+  );
+}
