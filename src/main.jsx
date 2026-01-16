fe55346c4311b794e260e3d6e2443982bd7c4e50
Cameron Stanley <cam96stanley@gmail.com>
2026-01-15 23:49:03 -0500

    set up pwa ability for app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./scss/index.scss";
 import { AuthProvider } from "./context/AuthProvider.jsx";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+    console.log("New version available");
+  },
+  onofflineReady() {
+    console.log("App ready to work offline");
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
